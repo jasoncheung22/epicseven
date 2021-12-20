6b6e9246d66e83ce8a6e533fef1e8b3a816b9445
jason <jason.cheung@tfs-game.com>
2021-12-20 10:03:04 +0800

    add artifact magic-for-friends

--- src/assets/js/tw.artifacts.js
+++ src/assets/js/tw.artifacts.js
@@ -70,6 +70,7 @@ export const tw = [
   { _id: 'justice-for-all', name: '\u70ba\u773e\u4eba\u7684\u6b63\u7fa9', rarity: 5, role: 'knight' },
   { _id: 'knowledge-seed', name: '\u77e5\u8b58\u7a2e\u5b50', rarity: 5, role: 'mage' },
   { _id: 'last-teatime', name: '\u6700\u5f8c\u7684\u4e0b\u5348\u8336', rarity: 5, role: 'mage' },
+  { _id: 'magic-for-friends', name: '\u70ba\u4e86\u670b\u53cb\u7684\u9b54\u6cd5', rarity: 5, role: 'mage' },
   { _id: 'manica-of-control', name: '\u63a7\u5236\u7684\u8b77\u81c2', rarity: 5, role: 'assassin' },
   { _id: 'manifu', name: '\u746a\u5c3c\u666e', rarity: 5, role: '' },
   { _id: 'merciless-glutton', name: '\u7121\u60c5\u7684\u5927\u98df\u5ba2', rarity: 5, role: 'warrior' },
